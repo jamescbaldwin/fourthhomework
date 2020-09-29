63ee2c2a1b162555218f550694772186003d36f3
James Baldwin <jamescbaldwin@gmail.com>
2020-09-29 11:23:12 -0400

    commit2

---
+++ script.js
@@ -0,0 +1,61 @@
+var nextQuestionEl = document.getElementById("#nextQuestion");
+var prevQuestionEl = document.getElementById("#prevQuestion");
+var answer1El = document.getElementById("#answer1");
+var answer2El = document.getElementById("#answer2");
+var answer3El = document.getElementById("#answer3");
+var answer4El = document.getElementById("#answer4");
+var startBtn = document.getElementById("#startbutton");
+var cardEl = document.getElementsByClassName(".card");
+var questionNumEl = document.getElementById("#questionNum");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Questions
+// 1 Complete the common coding analogy: CSS is to JavaScript, as Microsoft Word is to ____
+//     1. CALCULATOR
+//     2. POWERPOINT
+//     3. NOTEPAD
+//     4. WINDOWS 10
+
+//  2 Javascript was initially desinged by Brendan Eich, while a developer for which tech company?
+//     1. MICROSOFT
+//     2. APPLE
+//     3. MOZILLA
+//     4. NETSCAPE
+
+// 3 Which of the following is proper JavaScript notation for listing an array?
+//     1. SQUARE BRACKETS
+//     2. CURLY BRACKETS
+//     3. DOUBLE-QUOTES
+//     4. PARENTHESIS
+
+// 4 True/False values are otherwise known in Javascript as?
+//     1. BOOLEAN VALUES
+//     2. BINARY VALUES
+//     3. LOGIC VALUES
+//     4. PROPOSITIONAL VALUES
+
+
+
+
+
